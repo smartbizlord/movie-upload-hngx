410cd41f0dda278be6c917481e5ad3db7a67d589
unknown <smartbizlord@gmail.com>
2023-10-02 12:12:37 +0100

    separated concerns for the upload and final saving when the record button is stopped

--- controllers/video.controller.js
+++ controllers/video.controller.js
@@ -52,55 +52,59 @@ const recordAndSave = async (req, res) => {
     }
   
     const tempFilePath = `${tempDir}/${recordingId}_${chunkIndex}.chunk`;
-  
-    fs.writeFile(tempFilePath, videoChunk, 'base64', (err) => {
-      if (err) {
-        console.log(err, "there was a problem saving the chunk to disk")
-        return new ApiError(httpStatus.BAD_GATEWAY, "Movie Upload failed");
-      }
-  
-      console.log(`Received and saved chunk ${chunkIndex}`);
-  
-      if (!hasNextChunk) {
-        // No more chunks expected, send video for transcription
-        const tempFilePaths = fs.readdirSync(tempDir).filter(file => file.startsWith(`${recordingId}_`));
-        const chunks = tempFilePaths.map(filePath => fs.readFileSync(`${tempDir}/${filePath}`));
-        const completeRecording = Buffer.concat(chunks);
-  
-        // Save the complete recording
-        const outputFilePath = `${recordingsDir}/${recordingId}.mp4`;
-        fs.writeFile(outputFilePath, completeRecording, 'base64', (err) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-  
-          console.log(`Recording saved at ${outputFilePath}`);
-  
-          // Clean up temporary files
-          tempFilePaths.forEach(filePath => fs.unlinkSync(`${tempDir}/${filePath}`));
-  
-          // Connect to RabbitMQ and send video for transcription
-        //   amqp.connect(`amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`).then((connection) => {
-        //     return connection.createChannel().then((channel) => {
-        //       const queue = 'video_transcription';
-  
-        //       channel.assertQueue(queue, { durable: true });
-        //       channel.sendToQueue(queue, Buffer.from(outputFilePath)); // Send the file path
-  
-        //       console.log('Sent video for transcription');
-  
-        //       setTimeout(() => {
-        //         connection.close();
-        //       }, 500);
-        //     });
-        //   }).catch(console.warn);
-  
-          res.sendStatus(200);
-        });
-      } else {
+
+    if (!hasNextChunk) {
+      // No more chunks expected, send video for transcription
+      const tempFilePaths = fs.readdirSync(tempDir).filter(file => file.startsWith(`${recordingId}_`));
+      const chunks = tempFilePaths.map(filePath => fs.readFileSync(`${tempDir}/${filePath}`));
+      const completeRecording = Buffer.concat(chunks);
+
+      // Save the complete recording
+      const outputFilePath = `${recordingsDir}/${recordingId}.mp4`;
+      fs.writeFile(outputFilePath, completeRecording, 'base64', (err) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+
+        console.log(`Recording saved at ${outputFilePath}`);
+
+        // Clean up temporary files
+        tempFilePaths.forEach(filePath => fs.unlinkSync(`${tempDir}/${filePath}`));
+
+        // Connect to RabbitMQ and send video for transcription
+      //   amqp.connect(`amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`).then((connection) => {
+      //     return connection.createChannel().then((channel) => {
+      //       const queue = 'video_transcription';
+
+      //       channel.assertQueue(queue, { durable: true });
+      //       channel.sendToQueue(queue, Buffer.from(outputFilePath)); // Send the file path
+
+      //       console.log('Sent video for transcription');
+
+      //       setTimeout(() => {
+      //         connection.close();
+      //       }, 500);
+      //     });
+      //   }).catch(console.warn);
+
         res.sendStatus(200);
-      }
-    });
+      });
+    }
+  
+    if (videoChunk) {
+      fs.writeFile(tempFilePath, videoChunk, 'base64', (err) => {
+        if (err) {
+          console.log(err, "there was a problem saving the chunk to disk")
+          return new ApiError(httpStatus.BAD_GATEWAY, "Movie Upload failed");
+        }
+    
+        console.log(`Received and saved chunk ${chunkIndex}`);
+    
+         {
+          res.sendStatus(200);
+        }
+      });
+    }
   }
 
   const initializer = () => {}
